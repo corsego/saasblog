7dcba4d57debd4b4880b4584fc8413bfc19aedfc
Yaro <yshmarov@gmail.com>
2021-03-12 16:07:35 +0000

    assign stripe_customer_id to newly registered users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_212919) do
+ActiveRecord::Schema.define(version: 2021_03_12_160417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_212919) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "plan"
     t.string "subscription_status", default: "incomplete"
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
