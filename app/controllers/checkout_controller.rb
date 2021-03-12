044311dc77bb099f3f9aedeb54217ee2573e0981
Yaro <yshmarov@gmail.com>
2021-03-12 17:37:54 +0000

    current_user can select any available plan

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,11 +2,12 @@ class CheckoutController < ApplicationController
   
   def create
     @session = Stripe::Checkout::Session.create({
+      customer: current_user.stripe_customer_id,
       success_url: posts_url,
       cancel_url: pricing_url,
       payment_method_types: ['card'],
       line_items: [
-        {price: 'price_1IUDb0AkDVZc6qQweqsS6ATK', quantity: 1},
+        {price: params[:price], quantity: 1},
       ],
       mode: 'subscription',
     })
