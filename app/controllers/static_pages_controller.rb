7fcc8386ec9858adb57b006b35b0eab5994b1c71
Yaro <yshmarov@gmail.com>
2021-03-12 16:24:15 +0000

    display stripe pricing on static page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesController < ApplicationController
+  
+  def pricing
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
+  get "pricing", to: "static_pages#pricing"
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
