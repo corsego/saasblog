80a055f882a89ecf9ccb738c431399f5522d103a
Yaro <yshmarov@gmail.com>
2021-03-13 13:18:37 +0000

    final styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def pricing
+    @pricing = Stripe::Price.list(lookup_keys: ["good_year", "good_month"], expand: ["data.product"]).data.sort_by {|p| p.unit_amount}
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :title, :content, presence: true
 
   scope :free, -> { where(premium: false) }
+  scope :premium, -> { where(premium: true) }
   
   def to_s
     title
