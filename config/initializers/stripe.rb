6efabe03d26dfbb4032c0f0b02c6d8000573e5ab
Yaro <yshmarov@gmail.com>
2021-05-22 15:46:53 +0000

    clean up stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,1 @@
-if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret].present?
-  Stripe.api_key = Rails.application.credentials[:stripe][:secret]
-end
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret)
